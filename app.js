3104b6313a3bd00b3a111c8e6b7a84e78c714e92
Aaron Jacobowitz <amjacobowitz@gmail.com>
2015-08-01 14:23:47 -0700

    Add draw with color as variable and width as variable.  Add sockets. Need to send drawing through sockets, add buttons to select different colors and widths on template.

--- app.js
+++ app.js
@@ -1,47 +1,33 @@
-//this lets you parse the body of html requests to get the "params out"
-var bodyParser = require('body-parser')
-//this incorporates express into my node.js and then calls the function to make the app
-var app = require('express')();
-//this starts my server for my backend with the app as an input
-var http = require('http').Server(app)
-//this starts the sockets for this server
-var io = require('socket.io')(http);
+var bodyParser = require('body-parser');
+var path = require('path');
+var express = require('express');
+var app = express();
+var port = 3000;
+var io = require('socket.io').listen(app.listen(port));
+
+console.log("Listening on port " + port);
 
 
-//this is just a shorter way to set up the server to work with socket io
-// var io = require('socket.io').listen(server, function() {
-//         console.log("Express server listening on port " + app.get('port'));
-// });
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
 
-// app.use('/javascripts',express.static(path.join(__dirname, 'public/javascripts')));
+app.use('/javascripts', express.static(path.join(__dirname, 'public/javascripts')))
+app.use('/css', express.static(path.join(__dirname, 'public/stylesheets')));
+app.use('/images', express.static(path.join(__dirname, 'public/images')));
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// set the project to use jade as the template engine instead of basic html
-// I can't seem to get this to work with sockets though.  Not finding where I save the sockets.
-// app.set('view engine', 'jade');
-
-// these are how to write routes for the same base-route, i.e. get, post, delete, put
-app.route('/users')
+app.route('/')
   .get(function(req, res){
-    res.sendfile('index.html')
+    res.render('layout')
   })
   .post(function(req, res){
-    res.sendfile('index.html', {title: 'Hey', message: req.body.input} )
+    res.render('layout')
   })
 
-// this is how to access params from a route that uses a wildcard-type-thingy, but not from post
-app.get('/users/:user', function(req, res){
-  res.render('index', {title: 'Hey', message: 'hello there!', input: req.params.user})
-})
 
-//Connect to the Node.js Server
-io.on('connection', function(socket){
-  console.log('a user connected')
-})
-
-
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+app.get('/:user', function(req, res){
+  res.render('layout', {title: 'Hey', message: 'hello there!', input: req.params.user})
 })
 
 
@@ -51,25 +37,21 @@ http.listen(3000, function(){
 
 
 
+// io.on === io.sockets.on
+io.sockets.on('connection', function(socket){
+	socket.on('message', function(){
+		setInterval(function(){
+				socket.emit('time', {current_time: new Date()});
+		}, 1000);
+	})
+})
 
 
 
 
-// //send a ping event to the server from the client
-// console.log('socket: browser says ping (1)')
-// io.emit('ping', {some: data});
 
-// //how a client connects to the server (opens up a socket)
-// io.sockets.on('connection', function(socket){
-//   socket.on('ping', function(data){
-//     console.log('socket: server receives ping (2)');
-//   })
 
-//   socket.emit('pong', data)
-//   console.log('socket: server sends pong (3)')
-// })
 
-// //when the client receives a pong event from the server, console log the message
-// io.on('pong', function(data){
-//   console/log('socket: broswer received pong (4)', data)
-// })
+
+
+
---
+++ public/javascripts/socket.js
@@ -0,0 +1,85 @@
+// might need 3000, might not.  Not clear.
+var socket = io.connect('http://localhost:3000');
+socket.emit('message')
+socket.on('time', function(data){
+	$('.time').text(data.current_time);
+})
+
+$(document).ready(function() {
+	var canvas = document.getElementById("drawingCanvas");
+	var context = canvas.getContext('2d');
+	
+	var canvasOffset = $('#drawingCanvas').offset();
+
+	//I am offsetting here so that my x and y coordinated are what I would expect.  They should be 0 for x in the top right and 0 for y there.  However, y is currently starting at 20.  Not sure why.  I have just put a +20 for now to take care of the discrepency
+	var offsetX = canvasOffset.left
+	var offsetY = canvasOffset.top + 20
+	// this variable determines whether we will track where the mouse is moving or not.  When true, tracks. When false, does not.
+	var drawingOn = false
+	var lineColor = 'black'
+	var lineWidth = 5
+
+
+	function trackMouseDown(event){
+		// should call start draw
+
+		curMouseX = parseInt(event.clientX - offsetX);
+		curMouseY = parseInt(event.clientY - offsetY);
+		console.log('mousedown x: '+ curMouseX);
+		console.log('mousedown y: '+ curMouseY);
+
+		drawingOn = true;
+
+	}
+
+	function trackMouseUp(event){
+		// should call the stop draw 
+
+		//I don't think I care about the coordinates when the user stops drawing, but I kept these here for testing purposes.
+		// mouseX = parseInt(event.clientX - offsetX);
+		// mouseY = parseInt(event.clientY - offsetY);
+		// console.log('mouseup x: '+ curMouseX);
+		// console.log('mouseup y: '+ curMouseY);
+
+		drawingOn = false;
+	}
+
+	function trackMouseMove(event){
+		// draw onto the canvas
+		if (drawingOn === true){
+			prevMouseX = curMouseX
+			prevMouseY = curMouseY
+			curMouseX = parseInt(event.clientX - offsetX);
+			curMouseY = parseInt(event.clientY - offsetY);
+			console.log('mouse moved to x: '+ curMouseX);
+			console.log('mouse moved to y: '+ curMouseY);
+
+			draw();
+		}
+	}
+
+	function draw(){
+		context.beginPath();
+		context.lineWidth = lineWidth; //make this a variable set by pressing a button latter on
+		context.strokeStyle = lineColor; //make this a variable too
+		context.moveTo(prevMouseX, prevMouseY);
+		context.lineTo(curMouseX, curMouseY);
+		context.stroke();
+		context.closePath()
+	}
+
+	$('#drawingCanvas').on('mousedown', function(event){
+		trackMouseDown(event);
+	})
+
+	$('#drawingCanvas').on('mouseup', function(event){
+		trackMouseUp(event);
+	})
+
+	$('#drawingCanvas').on('mousemove', function(event){
+		trackMouseMove(event);
+	})
+
+
+
+});
