0f7aaf1340756dc15fb0af0c0b82d00600cdc2c5
Aaron Jacobowitz <amjacobowitz@gmail.com>
2015-08-02 15:40:57 -0700

    heroku

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var path = require('path');
 var express = require('express');
 var app = express();
 var port = 5000;
-var io = require('socket.io').listen(app.listen(port));
+var io = require('socket.io').listen(app.listen(process.env.PORT || port));
 
 
 
